8d1917a7a1369faf2548d3d2ee3666437d3ffa79
unknown <c.tabares15@hotmail.com>
2020-09-14 20:42:48 -0500

    14/09/2020

--- src/Mobile.js
+++ src/Mobile.js
@@ -6,16 +6,26 @@ import {
   useTransform,
   useAnimation,
   useElementScroll,
+  AnimatePresence,
 } from "framer-motion";
 import "./Mobile.css";
 import TopBar from "./TopBar";
 import useWindowDimensions from "./dimensions";
 import { isMobile } from "react-device-detect";
 import { useInView } from "react-intersection-observer";
+import Screen_2 from "./Screen_2";
+import Screen_1 from "./Screen_1";
 
 function Mobile() {
   const { height, width } = useWindowDimensions();
   const history = useHistory();
+
+  const ref_container = useRef();
+  const { scrollYProgress } = useElementScroll(ref_container);
+
+  const [color, setColor] = useState("#fff");
+  const [screen_1, setScreen_1] = useState(false);
+  const [screen_2, setScreen_2] = useState(false);
 
   //UX LABEL ANIMATION
 
@@ -87,6 +97,29 @@ function Mobile() {
     }
   }, [inView_ui_cont]);
 
+  //useEffect Screens and middle_circle
+
+  const control_middle_circle = useAnimation();
+
+  scrollYProgress.onChange((x) => {
+    if (x > 0 && x < 0.2) {
+      setScreen_1(false);
+      setColor("#fff");
+    } else if (x >= 0.2 && x < 0.6) {
+      setScreen_1(true);
+      setColor("#5d008e");
+      setScreen_2(false);
+      control_middle_circle.start("screen_1");
+    } else if (x >= 0.6) {
+      control_middle_circle.start("screen_2");
+
+      setScreen_1(false);
+      setColor("#fff");
+      setScreen_2(true);
+    }
+  });
+
+  //ANIMATION VARIANTS
   const initContainerVariants = {
     visible: {
       height: `${height >= width ? "100vh" : "100vw"}`,
@@ -102,8 +135,6 @@ function Mobile() {
       },
     },
   };
-
-  //ANIMATION VARIANTS
 
   const popup_squareVariants = {
     hidden: {
@@ -137,8 +168,52 @@ function Mobile() {
     },
   };
 
+  const screen_Variants = {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+
+    visible: {
+      opacity: 1,
+      scale: 1,
+
+      transition: {
+        duration: 0.5,
+        ease: "circOut",
+      },
+    },
+
+    exit: {
+      scale: 0,
+    },
+  };
+
+  const middle_circle_Variants = {
+    screen_0: {
+      rotate: [-20, 0, 20, 0, -20],
+      transition: { duration: 5, loop: "Infinity", ease: "linear" },
+    },
+    screen_1: {
+      transition: {
+        duration: 5,
+        loop: "Infinity",
+        ease: "linear",
+        y: { duration: 1 },
+      },
+    },
+
+    screen_2: {
+      y: 118,
+      transition: {
+        duration: 0.8,
+        ease: "linear",
+      },
+    },
+  };
+
   return (
-    <motion.div className="mobile_container" exit={{}}>
+    <motion.div className="mobile_container" ref={ref_container} exit={{}}>
       <motion.div
         variants={initContainerVariants}
         initial="visible"
@@ -180,7 +255,7 @@ function Mobile() {
           <path
             d="M132.43 54.5h0a1.44 1.44 0 0 1 1.42 1.42V87.63a1.44 1.44 0 0 1-2.88 0V56a1.43 1.43 0 0 1 1.41-1.45h0.05Z"
             transform="translate(0)"
-            fill="#0076c6"
+            fill="#ff6700"
             stroke="#000"
             stroke-miterlimit="10"
             stroke-width="0.5"
@@ -188,7 +263,7 @@ function Mobile() {
           <path
             d="M1.69 56.57h0A1.44 1.44 0 0 1 3.11 58V72.28a1.43 1.43 0 0 1-2.85 0V58a1.44 1.44 0 0 1 1.42-1.42Z"
             transform="translate(0)"
-            fill="#0076c6"
+            fill="#ff6700"
             stroke="#000"
             stroke-miterlimit="10"
             stroke-width="0.5"
@@ -196,7 +271,7 @@ function Mobile() {
           <path
             d="M1.69 78.22h0a1.43 1.43 0 0 1 1.42 1.42V93.93a1.43 1.43 0 0 1-2.85 0V79.64a1.43 1.43 0 0 1 1.42-1.42Z"
             transform="translate(0)"
-            fill="#0076c6"
+            fill="#ff6700"
             stroke="#000"
             stroke-miterlimit="10"
             stroke-width="0.5"
@@ -204,12 +279,13 @@ function Mobile() {
           <path
             d="M1.69 34.38h0A1.44 1.44 0 0 1 3.11 35.8V45a1.42 1.42 0 0 1-1.29 1.54h0A1.42 1.42 0 0 1 0.26 45.28h0a1 1 0 0 1 0-0.24V35.8a1.44 1.44 0 0 1 1.42-1.42Z"
             transform="translate(0)"
-            fill="#0076c6"
+            fill="#ff6700"
             stroke="#000"
             stroke-miterlimit="10"
             stroke-width="0.5"
           />
-          <rect
+
+          <motion.rect
             x="6.8"
             y="2.93"
             width="120.6"
@@ -218,266 +294,24 @@ function Mobile() {
             fill="#000000"
             id="content"
           />
-          {/* ////////////////////////////////////////////////////////////////////////// */}
 
-          <path
-            d="M24.45 2.4h85.4A17.63 17.63 0 0 1 127.45 20V243.9a17.63 17.63 0 0 1-17.6 17.6H24.45a17.63 17.63 0 0 1-17.6-17.6V20A17.63 17.63 0 0 1 24.45 2.4Z"
-            fill="#eaeaea"
-          />
-          <path
-            d="M125.85 61.3a36.7 36.7 0 1 0 0 73.4h1.6V61.3Z"
-            fill="#2a10c4"
-          />
-          <path
-            d="M16.35 30a36.91 36.91 0 0 0-9.5 1.2v70.9a36.91 36.91 0 0 0 9.5 1.2 36.65 36.65 0 1 0 0-73.3Z"
-            fill="#3e00ff"
-          />
-          <path
-            d="M16.35 134.7a36.91 36.91 0 0 0-9.5 1.2v70.9a36.91 36.91 0 0 0 9.5 1.2 36.65 36.65 0 1 0 0-73.3Z"
-            fill="#3d15ba"
-          />
-          <g isolation="isolate">
-            <text
-              transform="translate(8.9 63.09)"
-              font-size="8"
-              fill="#fff"
-              font-family="Bangers-Regular, Bangers"
-              isolation="isolate"
-            >
-              Wireframing
-            </text>
-            <text
-              transform="translate(8.9 63.09)"
-              font-size="8"
-              fill="none"
-              stroke="#2b2b2b"
-              stroke-miterlimit="10"
-              stroke-width="0.25"
-              font-family="Bangers-Regular, Bangers"
-              isolation="isolate"
-            >
-              Wireframing
-            </text>
-            <text
-              transform="translate(8.9 72.69)"
-              font-size="8"
-              fill="#fff"
-              font-family="Bangers-Regular, Bangers"
-              isolation="isolate"
-            >
-              (borrador)
-            </text>
-            <text
-              transform="translate(8.9 72.69)"
-              font-size="8"
-              fill="none"
-              stroke="#2b2b2b"
-              stroke-miterlimit="10"
-              stroke-width="0.25"
-              font-family="Bangers-Regular, Bangers"
-              isolation="isolate"
-            >
-              (borrador)
-            </text>
-          </g>
-          <g isolation="isolate">
-            <text
-              transform="translate(8.78 170.11)"
-              font-size="8"
-              fill="#fff"
-              font-family="Bangers-Regular, Bangers"
-              isolation="isolate"
-            >
-              A
-              <tspan x="3.46" y="0" letter-spacing="-0.02em">
-                R
-              </tspan>
-              <tspan x="6.82" y="0" letter-spacing="-0.01em">
-                Q
-              </tspan>
-              <tspan x="10.98" y="0">
-                U
-              </tspan>
-              <tspan x="14.33" y="0" letter-spacing="-0.01em">
-                I
-              </tspan>
-              <tspan x="15.89" y="0" letter-spacing="0em">
-                T
-              </tspan>
-              <tspan x="18.78" y="0" letter-spacing="-0.01em">
-                E
-              </tspan>
-              <tspan x="21.46" y="0">
-                CTURA
-              </tspan>
-            </text>
-            <text
-              transform="translate(8.78 170.11)"
-              font-size="8"
-              fill="none"
-              stroke="#2b2b2b"
-              stroke-miterlimit="10"
-              stroke-width="0.25"
-              font-family="Bangers-Regular, Bangers"
-              isolation="isolate"
-            >
-              A
-              <tspan x="3.46" y="0" letter-spacing="-0.02em">
-                R
-              </tspan>
-              <tspan x="6.82" y="0" letter-spacing="-0.01em">
-                Q
-              </tspan>
-              <tspan x="10.98" y="0">
-                U
-              </tspan>
-              <tspan x="14.33" y="0" letter-spacing="-0.01em">
-                I
-              </tspan>
-              <tspan x="15.89" y="0" letter-spacing="0em">
-                T
-              </tspan>
-              <tspan x="18.78" y="0" letter-spacing="-0.01em">
-                E
-              </tspan>
-              <tspan x="21.46" y="0">
-                CTURA
-              </tspan>
-            </text>
-            <text
-              transform="translate(23.18 179.71)"
-              font-size="8"
-              fill="#fff"
-              font-family="Bangers-Regular, Bangers"
-              isolation="isolate"
-            >
-              APP
-            </text>
-            <text
-              transform="translate(23.18 179.71)"
-              font-size="8"
-              fill="none"
-              stroke="#2b2b2b"
-              stroke-miterlimit="10"
-              stroke-width="0.25"
-              font-family="Bangers-Regular, Bangers"
-              isolation="isolate"
-            >
-              APP
-            </text>
-          </g>
-          <text
-            transform="translate(94.77 101.07)"
-            font-size="11.42"
-            fill="#fff"
-            font-family="Bangers-Regular, Bangers"
-            isolation="isolate"
-          >
-            DiSEÑO
-          </text>
-          <text
-            transform="translate(94.77 101.07)"
-            font-size="11.42"
-            fill="none"
-            stroke="#2b2b2b"
-            stroke-miterlimit="10"
-            stroke-width="0.25"
-            font-family="Bangers-Regular, Bangers"
-            isolation="isolate"
-          >
-            DiSEÑO
-          </text>
-          <path
-            d="M48.55 46.1l43.1 8.5s14.1 3-1.8 8.5l-22.1 8.6S43 81.2 65.45 90.8 88 100.3 88 100.3"
-            fill="none"
-            stroke="#2b2b2b"
-            stroke-linecap="round"
-            stroke-miterlimit="10"
-            stroke-width="2"
-            stroke-dasharray="3 4"
-          />
-          <path
-            d="M89 100.1a30 30 0 0 1-6.8-7l0.3 5.1-3 4A29.88 29.88 0 0 1 89 100.1Z"
-            fill="#2b2b2b"
-          />
-          <path
-            d="M52 177.8a30 30 0 0 1 6.3 7.5l0.1-5.1 3.3-3.8A33.94 33.94 0 0 1 52 177.8Z"
-            fill="#2b2b2b"
-          />
-          <path
-            d="M118.65 135.5s21.2 59.4-61.7 45.3"
-            fill="none"
-            stroke="#2b2b2b"
-            stroke-linecap="round"
-            stroke-miterlimit="10"
-            stroke-width="2"
-            stroke-dasharray="3 4"
-          />
-          <circle
-            cx="69.75"
-            cy="232.8"
-            r="27.4"
-            fill="none"
-            stroke="#fff"
-            stroke-linecap="round"
-            stroke-miterlimit="10"
-            stroke-dasharray="0 4"
-          />
-          <line
-            x1="68.65"
-            y1="205.5"
-            x2="68.65"
-            y2="260.2"
-            fill="none"
-            stroke="#fff"
-            stroke-miterlimit="10"
-            stroke-width="0.5"
-          />
-          <line
-            x1="42.45"
-            y1="232.8"
-            x2="97.15"
-            y2="232.8"
-            fill="none"
-            stroke="#fff"
-            stroke-miterlimit="10"
-            stroke-width="0.5"
-          />
-          <path
-            d="M76.55 245.8H61.05a6.7 6.7 0 0 1-6.7-6.7V223.5a6.7 6.7 0 0 1 6.7-6.7h15.5a6.7 6.7 0 0 1 6.7 6.7V239A6.79 6.79 0 0 1 76.55 245.8Z"
-            fill="#2b00ff"
-          />
-          <text
-            transform="translate(61.96 240.18)"
-            font-size="21.1"
-            fill="#fff"
-            font-family="Bangers-Regular, Bangers"
-            isolation="isolate"
-          >
-            !!
-          </text>
-          <text
-            transform="translate(61.96 240.18)"
-            font-size="21.1"
-            fill="none"
-            stroke="#2b2b2b"
-            stroke-miterlimit="10"
-            stroke-width="0.25"
-            font-family="Bangers-Regular, Bangers"
-            isolation="isolate"
-          >
-            !!
-          </text>
-          {/* ///////////////////////////////////////////////// */}
+          <AnimatePresence exitBeforeEnter>
+            {screen_1 ? <Screen_1 variants={screen_Variants} /> : null}
+            {screen_2 ? <Screen_2 variants={screen_Variants} /> : null}
+          </AnimatePresence>
+
           <motion.g
-            animate={{ rotate: [-20, 0, 20, 0, -20] }}
-            transition={{ duration: 5, loop: "Infinity", ease: "linear" }}
+            variants={middle_circle_Variants}
+            initial="screen_0"
+            animate={control_middle_circle}
+            // animate={{ rotate: [-20, 0, 20, 0, -20] }}
+            // transition={{ duration: 5, loop: "Infinity", ease: "linear" }}
           >
             <motion.circle
               cx="67.04"
               cy="131.65"
               r="6.83"
-              fill="#fff"
+              fill={color}
               animate={{ y: [0, -2], x: [0, 1] }}
               transition={{ yoyo: Infinity, duration: 2 }}
             />
@@ -485,7 +319,7 @@ function Mobile() {
               cx="56.97"
               cy="125.15"
               r="0.79"
-              fill="#fff"
+              fill={color}
               initial={{ y: -2, scale: 0, opacity: 1 }}
               animate={{
                 y: [-3, -4, -10],
@@ -502,7 +336,7 @@ function Mobile() {
               cx="73.08"
               cy="125.94"
               r="1.01"
-              fill="#fff"
+              fill={color}
               animate={{ scale: [1, 2, 1] }}
               transition={{ duration: 5, loop: Infinity }}
             />
@@ -510,7 +344,7 @@ function Mobile() {
               cx="60.21"
               cy="133.21"
               r="0.72"
-              fill="#fff"
+              fill={color}
               initial={{ opacity: 1 }}
               animate={{
                 opacity: [1, 1, 1, 0],
@@ -529,7 +363,7 @@ function Mobile() {
               cx="71.97"
               cy="136.79"
               r="0.79"
-              fill="#fff"
+              fill={color}
               initial={{ opacity: 1 }}
               animate={{
                 opacity: [1, 1, 1, 0],
@@ -604,6 +438,11 @@ function Mobile() {
         <div className="thing_close">
           <img src={require("./img/tabcoda_close.svg")} />
         </div>
+        {isMobile ? (
+          <div className="thing_open">
+            <img src={require("./img/tabcoda_open.svg")} />
+          </div>
+        ) : null}
       </div>
     </motion.div>
   );
---
+++ src/Screen_1.js
@@ -0,0 +1,261 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+function Screen_1({ variants }) {
+  return (
+    <motion.g
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <path
+        d="M24.45 2.4h85.4A17.63 17.63 0 0 1 127.45 20V243.9a17.63 17.63 0 0 1-17.6 17.6H24.45a17.63 17.63 0 0 1-17.6-17.6V20A17.63 17.63 0 0 1 24.45 2.4Z"
+        fill="#eaeaea"
+      />
+      <path d="M125.85 61.3a36.7 36.7 0 1 0 0 73.4h1.6V61.3Z" fill="#2a10c4" />
+      <path
+        d="M16.35 30a36.91 36.91 0 0 0-9.5 1.2v70.9a36.91 36.91 0 0 0 9.5 1.2 36.65 36.65 0 1 0 0-73.3Z"
+        fill="#3e00ff"
+      />
+      <path
+        d="M16.35 134.7a36.91 36.91 0 0 0-9.5 1.2v70.9a36.91 36.91 0 0 0 9.5 1.2 36.65 36.65 0 1 0 0-73.3Z"
+        fill="#3d15ba"
+      />
+      <g isolation="isolate">
+        <text
+          transform="translate(8.9 63.09)"
+          font-size="8"
+          fill="#fff"
+          font-family="Bangers-Regular, Bangers"
+          isolation="isolate"
+        >
+          Wireframing
+        </text>
+        <text
+          transform="translate(8.9 63.09)"
+          font-size="8"
+          fill="none"
+          stroke="#2b2b2b"
+          stroke-miterlimit="10"
+          stroke-width="0.25"
+          font-family="Bangers-Regular, Bangers"
+          isolation="isolate"
+        >
+          Wireframing
+        </text>
+        <text
+          transform="translate(8.9 72.69)"
+          font-size="8"
+          fill="#fff"
+          font-family="Bangers-Regular, Bangers"
+          isolation="isolate"
+        >
+          (borrador)
+        </text>
+        <text
+          transform="translate(8.9 72.69)"
+          font-size="8"
+          fill="none"
+          stroke="#2b2b2b"
+          stroke-miterlimit="10"
+          stroke-width="0.25"
+          font-family="Bangers-Regular, Bangers"
+          isolation="isolate"
+        >
+          (borrador)
+        </text>
+      </g>
+      <g isolation="isolate">
+        <text
+          transform="translate(8.78 170.11)"
+          font-size="8"
+          fill="#fff"
+          font-family="Bangers-Regular, Bangers"
+          isolation="isolate"
+        >
+          A
+          <tspan x="3.46" y="0" letter-spacing="-0.02em">
+            R
+          </tspan>
+          <tspan x="6.82" y="0" letter-spacing="-0.01em">
+            Q
+          </tspan>
+          <tspan x="10.98" y="0">
+            U
+          </tspan>
+          <tspan x="14.33" y="0" letter-spacing="-0.01em">
+            I
+          </tspan>
+          <tspan x="15.89" y="0" letter-spacing="0em">
+            T
+          </tspan>
+          <tspan x="18.78" y="0" letter-spacing="-0.01em">
+            E
+          </tspan>
+          <tspan x="21.46" y="0">
+            CTURA
+          </tspan>
+        </text>
+        <text
+          transform="translate(8.78 170.11)"
+          font-size="8"
+          fill="none"
+          stroke="#2b2b2b"
+          stroke-miterlimit="10"
+          stroke-width="0.25"
+          font-family="Bangers-Regular, Bangers"
+          isolation="isolate"
+        >
+          A
+          <tspan x="3.46" y="0" letter-spacing="-0.02em">
+            R
+          </tspan>
+          <tspan x="6.82" y="0" letter-spacing="-0.01em">
+            Q
+          </tspan>
+          <tspan x="10.98" y="0">
+            U
+          </tspan>
+          <tspan x="14.33" y="0" letter-spacing="-0.01em">
+            I
+          </tspan>
+          <tspan x="15.89" y="0" letter-spacing="0em">
+            T
+          </tspan>
+          <tspan x="18.78" y="0" letter-spacing="-0.01em">
+            E
+          </tspan>
+          <tspan x="21.46" y="0">
+            CTURA
+          </tspan>
+        </text>
+        <text
+          transform="translate(23.18 179.71)"
+          font-size="8"
+          fill="#fff"
+          font-family="Bangers-Regular, Bangers"
+          isolation="isolate"
+        >
+          APP
+        </text>
+        <text
+          transform="translate(23.18 179.71)"
+          font-size="8"
+          fill="none"
+          stroke="#2b2b2b"
+          stroke-miterlimit="10"
+          stroke-width="0.25"
+          font-family="Bangers-Regular, Bangers"
+          isolation="isolate"
+        >
+          APP
+        </text>
+      </g>
+      <text
+        transform="translate(94.77 101.07)"
+        font-size="11.42"
+        fill="#fff"
+        font-family="Bangers-Regular, Bangers"
+        isolation="isolate"
+      >
+        DiSEÑO
+      </text>
+      <text
+        transform="translate(94.77 101.07)"
+        font-size="11.42"
+        fill="none"
+        stroke="#2b2b2b"
+        stroke-miterlimit="10"
+        stroke-width="0.25"
+        font-family="Bangers-Regular, Bangers"
+        isolation="isolate"
+      >
+        DiSEÑO
+      </text>
+      <path
+        d="M48.55 46.1l43.1 8.5s14.1 3-1.8 8.5l-22.1 8.6S43 81.2 65.45 90.8 88 100.3 88 100.3"
+        fill="none"
+        stroke="#2b2b2b"
+        stroke-linecap="round"
+        stroke-miterlimit="10"
+        stroke-width="2"
+        stroke-dasharray="3 4"
+      />
+      <path
+        d="M89 100.1a30 30 0 0 1-6.8-7l0.3 5.1-3 4A29.88 29.88 0 0 1 89 100.1Z"
+        fill="#2b2b2b"
+      />
+      <path
+        d="M52 177.8a30 30 0 0 1 6.3 7.5l0.1-5.1 3.3-3.8A33.94 33.94 0 0 1 52 177.8Z"
+        fill="#2b2b2b"
+      />
+      <path
+        d="M118.65 135.5s21.2 59.4-61.7 45.3"
+        fill="none"
+        stroke="#2b2b2b"
+        stroke-linecap="round"
+        stroke-miterlimit="10"
+        stroke-width="2"
+        stroke-dasharray="3 4"
+      />
+      <circle
+        cx="69.75"
+        cy="232.8"
+        r="27.4"
+        fill="none"
+        stroke="#fff"
+        stroke-linecap="round"
+        stroke-miterlimit="10"
+        stroke-dasharray="0 4"
+      />
+      <line
+        x1="68.65"
+        y1="205.5"
+        x2="68.65"
+        y2="260.2"
+        fill="none"
+        stroke="#fff"
+        stroke-miterlimit="10"
+        stroke-width="0.5"
+      />
+      <line
+        x1="42.45"
+        y1="232.8"
+        x2="97.15"
+        y2="232.8"
+        fill="none"
+        stroke="#fff"
+        stroke-miterlimit="10"
+        stroke-width="0.5"
+      />
+      <path
+        d="M76.55 245.8H61.05a6.7 6.7 0 0 1-6.7-6.7V223.5a6.7 6.7 0 0 1 6.7-6.7h15.5a6.7 6.7 0 0 1 6.7 6.7V239A6.79 6.79 0 0 1 76.55 245.8Z"
+        fill="#2b00ff"
+      />
+      <text
+        transform="translate(61.96 240.18)"
+        font-size="21.1"
+        fill="#fff"
+        font-family="Bangers-Regular, Bangers"
+        isolation="isolate"
+      >
+        !!
+      </text>
+      <text
+        transform="translate(61.96 240.18)"
+        font-size="21.1"
+        fill="none"
+        stroke="#2b2b2b"
+        stroke-miterlimit="10"
+        stroke-width="0.25"
+        font-family="Bangers-Regular, Bangers"
+        isolation="isolate"
+      >
+        !!
+      </text>
+    </motion.g>
+  );
+}
+
+export default Screen_1;
